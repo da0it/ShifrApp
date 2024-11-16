be9d54c882201036b984105607a0d951d7142d95
Dai0bit <nikulas.nikulin@yandex.ru>
2024-11-16 18:24:29 +0300

    Setted up raw kuznechik

--- ShifrApp/obj/Debug/net8.0/ShifrApp.AssemblyInfo.cs
+++ ShifrApp/obj/Debug/net8.0/ShifrApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShifrApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f087cb3115b6deb77a72857111b27681b23919e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93e5a7df15283494530d46955b492af2a0395c52")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShifrApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShifrApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
