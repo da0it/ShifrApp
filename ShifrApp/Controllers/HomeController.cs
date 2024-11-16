f944e380b1f53c4d20b0316b86c8d3ad44227b29
dddd <77583282+da0it@users.noreply.github.com>
2024-11-16 18:28:33 +0300

    Revert "Added feature"

--- ShifrApp/Controllers/HomeController.cs
+++ ShifrApp/Controllers/HomeController.cs
@@ -5,8 +5,6 @@ using ShifrApp.Models;
 using System.Security.Cryptography;
 using System.Text;
 using System.Runtime.Intrinsics.Arm;
-using ShifrApp.cipher;
-
 
 namespace ShifrApp.Controllers
 {
@@ -32,7 +30,7 @@ namespace ShifrApp.Controllers
         {
             if (!string.IsNullOrEmpty(model.Input))
             {
-                model.EncryptedString = EncryptGrassHopper2(model.Input);
+                model.EncryptedString = EncryptSha256(model.Input);
             }
             else
             {
@@ -48,25 +46,23 @@ namespace ShifrApp.Controllers
 			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 		}
 
-        private static string EncryptGrassHopper2(string text) //Заглушка - на вход всегда подается тестовое значение для проверки корректности работы шифра
-		{
-            byte[] result = System.Convert.FromHexString(text);
-            byte[] padded_text = PaddArray(result);
-            Array.Reverse(padded_text);
-            string out_data = cipher.Kuznechik.KuznechikEncrypt(padded_text);
-			return out_data;
-        }
 
-        //Добавление нулей в массив байтов, если длина сообщения меньше BLOCK_SIZE
-        static byte[] PaddArray(byte[] bytes)
-        {
-            if (bytes.Length < 16)
-            {
-                byte[] paddedBytes = new byte[16];
-                Array.Copy(bytes, paddedBytes, bytes.Length);
-                return paddedBytes;
-            }
-            return bytes;
-        }
-    }
+
+		private string EncryptSha256(string rawData)
+		{
+			using (SHA256 sha256Hash = SHA256.Create())
+			{
+				//вычисление хэша
+				byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+				// Преобразование байтов в строку (hex формат)
+				StringBuilder builder = new StringBuilder();
+				for (int i = 0; i < bytes.Length; i++)
+				{
+					builder.Append(bytes[i].ToString("x2"));
+				}
+				return builder.ToString();
+			}
+		}
+	}
 }
--- ShifrApp/obj/Debug/net8.0/ShifrApp.AssemblyInfo.cs
+++ ShifrApp/obj/Debug/net8.0/ShifrApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShifrApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93e5a7df15283494530d46955b492af2a0395c52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShifrApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShifrApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
